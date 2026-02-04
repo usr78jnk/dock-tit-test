32b4c4c36f0c9cd5d1eae2db32c093ad63e9b010
Tot junior <you@example.com>
2026-02-04 17:19:12 +0100

    Modyfi app_api for docker

--- app_api.py
+++ app_api.py
@@ -90,7 +90,8 @@ if __name__ == "__main__":
 # uvicorn.run(app, host="127.0.0.1", port=5000, reload=True)
     uvicorn.run(
         "app_api:app",
-        host="127.0.0.1",
-        port=5000,
-        reload=True
+        # host="127.0.0.1",
+        host="0.0.0.0",
+        port=5000
+        # reload=True # for docker image this function doesnt need
     )
